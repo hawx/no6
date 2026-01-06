1cc87fd63a9090fd7db755b8184fb8dac800820b
Joshua Hawxwell <m@hawx.me>
2026-01-06 11:11:25 +0000

    Add a little no6csv tool to help play with data

---
+++ cmd/no6csv/main.go
@@ -0,0 +1,89 @@
+// Command no6csv allows importing/exporting data to/from a no6 database from/to a csv file.
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"hawx.me/code/no6"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "usage: no6csv in PATH < FILE.csv\n       no6csv out PATH > FILE.csv")
+		os.Exit(2)
+		return
+	}
+
+	switch os.Args[1] {
+	case "in":
+		if err := runIn(os.Args[2]); err != nil {
+			fmt.Fprintln(os.Stderr, "in error: "+err.Error())
+			os.Exit(1)
+			return
+		}
+	case "out":
+		if err := runOut(os.Args[2]); err != nil {
+			fmt.Fprintln(os.Stderr, "out error: "+err.Error())
+			os.Exit(1)
+			return
+		}
+	case "help":
+		fmt.Fprintln(os.Stderr, "usage: no6csv in|out PATH")
+		os.Exit(2)
+		return
+	}
+}
+
+func runIn(path string) error {
+	store, err := no6.Open(path)
+	if err != nil {
+		return err
+	}
+
+	r := csv.NewReader(os.Stdin)
+	line := 0
+
+	for {
+		record, err := r.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			return err
+		}
+
+		if len(record) != 3 {
+			return fmt.Errorf("csv must contain lines with 3 fields, got %d on line %d", len(record), line)
+		}
+
+		if err := store.Put(record[0], record[1], record[2]); err != nil {
+			return err
+		}
+
+		line++
+	}
+}
+
+func runOut(path string) error {
+	store, err := no6.Open(path)
+	if err != nil {
+		return err
+	}
+
+	triples := store.Query()
+	w := csv.NewWriter(os.Stdout)
+
+	for _, triple := range triples {
+		if err := w.Write([]string{triple.Subject, triple.Predicate, fmt.Sprint(triple.Object)}); err != nil {
+			return err
+		}
+	}
+
+	w.Flush()
+	return w.Error()
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,14 @@
 module hawx.me/code/no6
 
-go 1.22.0
+go 1.24.0
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	go.etcd.io/bbolt v1.3.11 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	hawx.me/code/assert v0.0.0-20200428180912-91e855e32e7d // indirect
+	github.com/google/uuid v1.6.0
+	go.etcd.io/bbolt v1.3.11
+	hawx.me/code/assert v0.0.0-20200428180912-91e855e32e7d
+)
+
+require (
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 )
